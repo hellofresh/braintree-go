18e05b16fadb954e2a28045a08db33bbcb87321f
marija.trajkova <marija.trajkova@hellofresh.com>
2024-05-27 17:20:52 +0200

    Revert test

--- transaction_venmo_account_details_integration_test.go
+++ transaction_venmo_account_details_integration_test.go
@@ -52,57 +52,6 @@ func TestTransactionVenmoAccountDetails(t *testing.T) {
 	}
 }
 
-const profileId = "venmoProfileId"
-
-func TestTransactionVenmoAccountDetailsWithOptions(t *testing.T) {
-	ctx := context.Background()
-
-	tx, err := testGateway.Transaction().Create(ctx, &TransactionRequest{
-		Type:               "sale",
-		Amount:             NewDecimal(2000, 2),
-		PaymentMethodNonce: FakeNonceVenmoAccount,
-		Options: &TransactionOptions{
-			TransactionOptionsVenmoRequest: &TransactionOptionsVenmoRequest{
-				ProfileId: profileId,
-			},
-		},
-	})
-
-	t.Log(tx)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-	if tx.Id == "" {
-		t.Fatal("Received invalid ID on new transaction")
-	}
-	if tx.Status != TransactionStatusAuthorized {
-		t.Fatal(tx.Status)
-	}
-
-	if tx.VenmoAccountDetails == nil {
-		t.Fatal("Expected VenmoAccountDetails for transaction created with VenmoAccount nonce")
-	}
-
-	t.Log(tx.VenmoAccountDetails)
-
-	if tx.VenmoAccountDetails.Token != "" {
-		t.Fatal("Expected VenmoAccountDetails to not have Token set")
-	}
-	if tx.VenmoAccountDetails.Username == "" {
-		t.Fatal("Expected VenmoAccountDetails to have Username set")
-	}
-	if tx.VenmoAccountDetails.VenmoUserID == "" {
-		t.Fatal("Expected VenmoAccountDetails to have VenmoUserID set")
-	}
-	if tx.VenmoAccountDetails.SourceDescription == "" {
-		t.Fatal("Expected VenmoAccountDetails to have SourceDescription set")
-	}
-	if tx.VenmoAccountDetails.ImageURL == "" {
-		t.Fatal("Expected VenmoAccountDetails to have ImageURL set")
-	}
-}
-
 func TestTransactionWithoutVenmoAccountDetails(t *testing.T) {
 	ctx := context.Background()
 
